765db559837baa5c0ee22f2f1559937f053dbe78
YawDev <94807175+YawDev@users.noreply.github.com>
2025-03-23 13:21:44 -0400

    AddRoutes, SignUp Page

---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,12 @@
+import FormComponent from "../Components/FormComponent"
+
+
+function Signup() {
+    return (
+        <div>
+            <FormComponent />
+         </div>
+     )
+}
+
+export default Signup
